3e83543c6c4e595c6706b44e7f44adfc861c2551
Taufan Hidayatul Akbar <146913216+Taufan-Dev@users.noreply.github.com>
2024-11-30 02:12:17 +0700

    Add files via upload

---
+++ code-m4&5/js/jsM43.js
@@ -0,0 +1,10 @@
+function pesan() {
+  alert("ini latisan javascript pertama saya");
+}
+function pesanConsole() {
+  console.log("its work");
+}
+function pesanDok() {
+  document.writeln("Success");
+  document.writeln("<a href=m4-2.html>kembali</a>");
+}
